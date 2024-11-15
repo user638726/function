b29e841f84b1398e2338ff73d3c345aa288b1b42
user638726 <lienjackson35@gmail.com>
2024-11-15 11:17:05 +0800

    增加菱形

--- function.php
+++ function.php
@@ -3,10 +3,12 @@
    font-family:'courier new'
 }
 </style>
-<h2>正三角形</h2>
+
     <?php
-    function starts($line){
-    for ($i=0;$i<$line;$i++){
+    function starts($shape,$line){
+      switch($shape){
+    case "正三角形":
+      for ($i=0;$i<$line;$i++){
       for($k=0;$k<$line-1-$i;$k++){
         echo "&nbsp;";
         //0 1 2 3 4 是星星旁邊的空格(由上至下)
@@ -16,6 +18,30 @@
         //1 3 5 7 9　星星數
     }
         echo "<br>";
-    }    
+    }
+    break;
+    case "菱形";
+    for($i=0;$i<$line;$i++){
+      if($i>floor($line/2)){
+          $k1=$i-(floor($line/2));
+          $j1=2*($i-(2*($i-(floor($line/2)))))+1;
+      }else{
+          $k1=(floor($line/2))-$i;
+          $j1=(2*$i+1);
+      }
+  
+      for($k=0;$k<$k1;$k++){
+          echo "&nbsp;";
+      }
+  
+      for($j=0;$j<$j1;$j++){
+          echo "*";
+      }
+      echo "<br>";
+  
+  }
+    
+  break;
+    }
     }
     ?>
