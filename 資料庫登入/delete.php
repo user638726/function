bf8be7c82f773d64e91624282317f67fcfac596d
user638726 <lienjackson35@gmail.com>
2024-11-15 14:48:36 +0800

    增加del函式

--- 資料庫登入/delete.php
+++ 資料庫登入/delete.php
@@ -3,14 +3,10 @@ if(!isset($_GET['id']) || !is_numeric($_GET['id'])){
     echo "非法使用";
     exit();
 }
-
-$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
-$pdo=new PDO($dsn,'root','');
+include "../function.php";
 
 $id=$_GET['id'];
-$sql="delete from member where id='$id'";
+del('member',$id);
 
-$pdo->exec($sql);
 
 header("location:success.php");
-?>
